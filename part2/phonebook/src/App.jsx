d81889e4dd0b9fbc95a3cf033110c71f6f6233fd
andrey-krendzel-new <andrey.pandaoy@gmail.com>
2024-11-21 12:47:27 +0200

    Fixed 2.10 (was incomplete). Done 2.11)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({value, onChange}) => {
   return(<p>filter shown with <input 
@@ -27,16 +28,24 @@ const PersonForm = ({addPerson, newName, handleNameChange, newNumber, handleNumb
   </form>)
 }
 
+const Persons = ({persons, filterText}) => {
+  let numbers = persons.filter(person => person.name.toLowerCase().includes(filterText.toLowerCase())).map(person => <p key={person.id}>{person.name} {person.number}</p>)
+  return(numbers)
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterText, setFilterText] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -73,7 +82,7 @@ const App = () => {
       <h2>Add new</h2>
       <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      {persons.filter(person => person.name.toLowerCase().includes(filterText.toLowerCase())).map(person => <p>{person.name} {person.number}</p>)}
+      <Persons persons={persons} filterText={filterText} />
     </div>
   )
 }
--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Note = ({ note }) => {  
   return (    
@@ -6,11 +7,23 @@ const Note = ({ note }) => {
 )}
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
     'a new note...'
   )
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   const notesToShow = showAll    ? notes    : notes.filter(note => note.important === true)
 
